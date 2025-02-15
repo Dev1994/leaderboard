74d05da09f63d55a9adf88150337ec0ff44ba220
Devon Volkwyn <devtuks@gmail.com>
2025-02-15 14:05:34 +0100

    Responsive design

--- frontend/src/components/LeaderboardComponent.tsx
+++ frontend/src/components/LeaderboardComponent.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import { useAddPlayerMutation } from "../hooks/use-add-player-mutation.ts";
-import { useNavigate } from "react-router";
-import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
-import { InputText } from "primereact/inputtext";
-import { Button } from "primereact/button";
-import { Fieldset } from "primereact/fieldset";
-import { useLeaderboardQuery } from "../hooks/use-leaderboard-query.ts";
-import { DateTime } from "luxon";
-import { Dropdown } from "primereact/dropdown";
+import {useEffect, useState} from "react";
+import {useAddPlayerMutation} from "../hooks/use-add-player-mutation.ts";
+import {useNavigate} from "react-router";
+import {Column} from "primereact/column";
+import {DataTable} from "primereact/datatable";
+import {InputText} from "primereact/inputtext";
+import {Button} from "primereact/button";
+import {Fieldset} from "primereact/fieldset";
+import {useLeaderboardQuery} from "../hooks/use-leaderboard-query.ts";
+import {DateTime} from "luxon";
+import {Dropdown} from "primereact/dropdown";
 
 const items = [
     {name: "All Time", value: 0},
@@ -55,7 +55,7 @@ export function LeaderboardComponent() {
     }, [value]);
 
     return (
-        <div className="flex flex-col space-y-4">
+        <div className="flex flex-col space-y-4 sm:max-w-6xl sm:justify-self-center">
             <Fieldset legend={"Push-ups leaderboard"}>
                 <div className="flex space-x-4">
                     <Dropdown
@@ -82,7 +82,8 @@ export function LeaderboardComponent() {
                 <div className="p-inputgroup flex-1">
                     <InputText placeholder="Name" value={playerName}
                                onChange={(event) => setPlayerName(event.target.value)}/>
-                    <Button label="Add Player" disabled={playerName.length === 0} onClick={() => addPlayerMutation.mutate(playerName)}/>
+                    <Button label="Add Player" disabled={playerName.length === 0}
+                            onClick={() => addPlayerMutation.mutate(playerName)}/>
                 </div>
             </Fieldset>
         </div>
--- frontend/src/components/PlayerComponent.tsx
+++ frontend/src/components/PlayerComponent.tsx
@@ -1,16 +1,16 @@
-import { useParams } from "react-router";
-import { usePlayerQuery } from "../hooks/use-player-query.ts";
-import { InputNumber, InputNumberValueChangeEvent } from "primereact/inputnumber";
-import { useState } from "react";
-import { Button } from "primereact/button";
-import { useAddWorkoutMutation } from "../hooks/use-add-workout-mutation.ts";
-import { Divider } from "primereact/divider";
-import { BreadCrumb } from "primereact/breadcrumb";
-import { usePlayerWorkoutsQuery } from "../hooks/use-player-workouts-query.ts";
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
-import { Workout } from "../entities/workout.ts";
-import { DateTime } from "luxon";
+import {useParams} from "react-router";
+import {usePlayerQuery} from "../hooks/use-player-query.ts";
+import {InputNumber, InputNumberValueChangeEvent} from "primereact/inputnumber";
+import {useState} from "react";
+import {Button} from "primereact/button";
+import {useAddWorkoutMutation} from "../hooks/use-add-workout-mutation.ts";
+import {Divider} from "primereact/divider";
+import {BreadCrumb} from "primereact/breadcrumb";
+import {usePlayerWorkoutsQuery} from "../hooks/use-player-workouts-query.ts";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import {Workout} from "../entities/workout.ts";
+import {DateTime} from "luxon";
 
 export function PlayerComponent() {
     const {id} = useParams();
@@ -46,28 +46,37 @@ export function PlayerComponent() {
         <div className="flex flex-col space-y-4">
             <BreadCrumb home={{icon: 'pi pi-home', url: '/'}} model={[{label: player.name}]}/>
 
-            <h1>{player.name}</h1>
-            <p>Total push-ups: {player.totalPushUps}</p>
-            <Divider/>
-            <InputNumber
-                value={pushUps}
-                onValueChange={(e: InputNumberValueChangeEvent) => setPushUps(e.value ?? 0)}
-                showButtons
-                buttonLayout="horizontal"
-                step={1}
-                decrementButtonClassName="p-button-danger"
-                incrementButtonClassName="p-button-success"
-                incrementButtonIcon="pi pi-plus"
-                decrementButtonIcon="pi pi-minus"
-                mode="decimal"/>
+            <div className="md:grid md:grid-cols-10">
+                <div className="flex flex-col space-y-4 md:flex md:flex-col md:col-span-4">
+                    <h1>{player.name}</h1>
+                    <p>Total push-ups: {player.totalPushUps}</p>
 
-            <Button label="Add push-ups" disabled={pushUps < 1} onClick={addPushUps}/>
+                    <InputNumber
+                        value={pushUps}
+                        onValueChange={(e: InputNumberValueChangeEvent) => setPushUps(e.value ?? 0)}
+                        showButtons
+                        buttonLayout="horizontal"
+                        step={1}
+                        decrementButtonClassName="p-button-danger"
+                        incrementButtonClassName="p-button-success"
+                        incrementButtonIcon="pi pi-plus"
+                        decrementButtonIcon="pi pi-minus"
+                        mode="decimal"/>
 
-            {playerWorkouts &&
-                <DataTable value={playerWorkouts} sortField={"createdAt"} sortOrder={-1} >
-                    <Column field={"pushUps"} header={"Push-ups"} sortable />
-                    <Column field={"createdAt"} header={"Created At"} body={formatDate} sortable />
-                </DataTable>}
+                    <Button label="Add push-ups" disabled={pushUps < 1} onClick={addPushUps}/>
+                </div>
+
+                <Divider/>
+
+                <div className="md:col-span-5">
+                    {playerWorkouts &&
+                        <DataTable value={playerWorkouts} sortField={"createdAt"} sortOrder={-1}>
+                            <Column field={"pushUps"} header={"Push-ups"} sortable/>
+                            <Column field={"createdAt"} header={"Created At"} body={formatDate} sortable/>
+                        </DataTable>}
+                </div>
+
+            </div>
         </div>
     )
 }
